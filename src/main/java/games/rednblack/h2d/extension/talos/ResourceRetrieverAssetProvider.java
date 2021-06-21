a0e326a3005717d62946f80bae58a2c5c49dc320
qlang <1224076583@qq.com>
2021-06-21 22:26:18 +0800

    add index region support.

--- src/main/java/games/rednblack/h2d/extension/talos/ResourceRetrieverAssetProvider.java
+++ src/main/java/games/rednblack/h2d/extension/talos/ResourceRetrieverAssetProvider.java
@@ -4,30 +4,31 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.talosvfx.talos.runtime.assets.BaseAssetProvider;
+
 import games.rednblack.editor.renderer.resources.IResourceRetriever;
 
 public class ResourceRetrieverAssetProvider extends BaseAssetProvider {
     private final IResourceRetriever resourceRetriever;
 
-    public ResourceRetrieverAssetProvider (final IResourceRetriever resourceRetriever) {
+    public ResourceRetrieverAssetProvider(final IResourceRetriever resourceRetriever) {
         this.resourceRetriever = resourceRetriever;
 
         setAssetHandler(TextureRegion.class, new AssetHandler<TextureRegion>() {
             @Override
-            public TextureRegion findAsset (String assetName) {
-                return resourceRetriever.getTextureRegion(assetName);
+            public TextureRegion findAsset(String assetName) {
+                return resourceRetriever.getTextureRegion(assetName, -1);
             }
         });
 
         setAssetHandler(Sprite.class, new AssetHandler<Sprite>() {
             @Override
-            public Sprite findAsset (String assetName) {
-                return newSprite((TextureAtlas.AtlasRegion) resourceRetriever.getTextureRegion(assetName));
+            public Sprite findAsset(String assetName) {
+                return newSprite((TextureAtlas.AtlasRegion) resourceRetriever.getTextureRegion(assetName, -1));
             }
         });
     }
 
-    private Sprite newSprite (TextureAtlas.AtlasRegion region) {
+    private Sprite newSprite(TextureAtlas.AtlasRegion region) {
         if (region.packedWidth == region.originalWidth && region.packedHeight == region.originalHeight) {
             if (region.rotate) {
                 Sprite sprite = new Sprite(region);
